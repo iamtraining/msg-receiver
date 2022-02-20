592abbd7797a1dd01c2eaebf92726f3302c1a9ea
Шамонин Денис <shamonin.denis@wildberries.ru>
2022-02-20 15:50:28 +0300

    splitting up code noodles

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![allow(unused_imports)]
 #![allow(unused_variables)]
 
+use failure::Fallible;
 use tokio::sync::mpsc;
 
 mod msg;
@@ -11,16 +12,25 @@ mod time;
 use time::my_sleep;
 
 mod msg_generator;
-use msg_generator::new_generator;
+use msg_generator as msggen;
 
 mod msg_receiver;
-use msg_receiver::new_receiver;
+use msg_receiver as msgrecv;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Fallible<()> {
     let (tx, rx) = mpsc::channel::<Msg>(10);
+    let (ctx, crx) = mpsc::channel::<msggen::Ctrl>(10);
+    tokio::spawn(msggen::new_generator(tx, crx));
+    tokio::spawn(msgrecv::new_receiver(rx));
 
-    tokio::spawn(new_generator(tx));
-    tokio::spawn(new_receiver(rx));
-    my_sleep(2000).await;
+    my_sleep(2000).await; // печатать сообщения
+
+    println!("sending quitting message");
+    ctx.send(msggen::Ctrl::Quit).await?; // выйти
+    println!("sending message have been sent");
+
+    my_sleep(2000).await; // не печатать сообщения
+
+    Ok(())
 }
--- src/msg_generator.rs
+++ src/msg_generator.rs
@@ -1,22 +1,40 @@
 use crate::{time::my_sleep, Msg};
+use tokio::select;
 use tokio::sync::mpsc::{Receiver, Sender};
 use tokio::sync::{mpsc, oneshot};
 
-pub async fn new_generator(chan: Sender<Msg>) {
+#[derive(Debug)]
+pub enum Ctrl {
+	Quit,
+}
+
+pub async fn new_generator(chan: Sender<Msg>, mut controller: Receiver<Ctrl>) {
 	loop {
-		if let Err(err) = chan.send(Msg::Den).await {
-			eprintln!("error while sending message{}", err);
-			break;
-		};
-		match chan.send(Msg::Nis).await {
-			Ok(()) => {
-				println!("ok")
+		tokio::select! {
+			msg = chan.send(Msg::Den) => {
+				match msg {
+					Ok(()) => my_sleep(100).await,
+					Err(err) => {
+						eprintln!("error while sending the message. error {}", err);
+						break;
+					}
+				}
 			}
-			Err(err) => {
-				eprintln!("error while sending message{}", err);
-				break;
+			msg = chan.send(Msg::Nis) => {
+				if let Err(err) = msg {
+					eprintln!("error while sending the message. error {}", err);
+					break;
+				}
 			}
-		};
-		my_sleep(200).await;
+			ctrl = controller.recv() => {
+				match ctrl {
+					Some(Ctrl::Quit) => {
+						println!("got a quit message");
+						break
+					},
+					None => break, // all senders have dropped
+				}
+			}
+		}
 	}
 }
